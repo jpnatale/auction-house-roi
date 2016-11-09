225e8346ef98034ad88f7d3aa534dfd0e00b3be6
Joe Natale <jpnatale@gmail.com>
2016-11-09 14:17:12 -0500

    first commit

---
+++ items.js
@@ -0,0 +1,33 @@
+
+module.exports = function(){
+	var itemIDS = [124105, 124101, 124102, 124104, 124103, 142117, 124442, 124441, 124440, 124444, 124106, 127850, 127849, 127848, 127847, 127842, 128542, 128543, 128541, 128548, 128550, 128549]
+
+	var itemsToGet = {
+		"124105": "Starlight Rose",
+		"124101": "Aethril",
+		"124102": "Dreamleaf",
+		"124104": "Fjarnskaggl",
+		"124103": "Foxflower",
+		"142117": "Blood of Sargeras (10x Prolonged Power)",
+		"124442": "Chaos Crystal",
+		"124441": "Leylight Shard",
+		"124440": "Arkhana",
+		"124444": "Infernal Brimstone",
+		"124106": "Felwort",
+		"127850": "Flask of Ten Thousand Scars",
+		"127849": "Flask of the Countless Armies",
+		"127848": "Flask of the Sevent Demon",
+		"127847": "Flask of the Whispered Pack",
+		"127842": "Infernal Alchemist Stone",
+		"128542": "Ring: Binding of Haste",
+		"128543": "Ring: Binding of Mastery",
+		"128541": "Ring: Binding of Critical Strike",
+		"128548": "Cloak: Binding of Strength",
+		"128550": "Cloak: Binding of Intellect",
+		"128549": "Cloak: Binding of Agilty"}
+
+	var items = [itemsToGet,itemIDS]
+
+	return items
+
+}
---
+++ run.js
@@ -0,0 +1,65 @@
+var ahdump = require("./ahdump.js")
+var ahurl = require("./AHURL.js")
+var ahData = require("./AHData.js")
+var items = require("./items.js")
+
+var itemIDs = items()[1]
+var itemsToGet = items()[0]
+var unitPrices = itemsToGet
+
+var test = ahurl().then(function(dataURLRes){
+
+	
+	console.log("Got Auction House URL")
+	return ahData(dataURLRes.files[0].url)
+
+}).then(function(data){
+	console.log('Retrieved Auction House Data')
+
+	for (var i = 0, len = itemIDs.length; i < len; i++) {
+		var foundUnit = findUnit(data,itemIDs[i])
+		var itemName = itemsToGet[itemIDs[i]]
+		unitPrices[itemIDs[i]] = foundUnit 
+		console.log(itemName +" - " + foundUnit)
+		
+	}
+	console.log(unitPrices)
+})
+
+		
+
+
+function findUnit (data,itemID){
+
+var matches = []
+
+for (var i = 0, len = data.auctions.length; i < len; i++) {
+  
+if(data.auctions[i].item == itemID){
+	var unit = data.auctions[i].buyout/data.auctions[i].quantity
+
+	if (unit>0){
+
+		matches.push(Math.round(unit/10000))}
+
+}}
+
+if (matches.length>1){
+matches = matches.sort( function(a,b) { return a - b; } )} else{
+	matches = "Not enough auctions available"
+}
+
+var finalUnit = 0
+if(matches.length<6){
+	finalUnit = (matches[0]+matches[1])/2
+}
+else if(matches.length>5){
+	finalUnit = Math.round((matches[0]+matches[1]+matches[2]+matches[3])/4)
+
+}
+
+return finalUnit
+
+
+}
+
