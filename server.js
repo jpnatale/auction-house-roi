9493c4c2800df399c4a7be66e1fcf4c41d2024ca
Joe Natale <jpnatale@gmail.com>
2016-11-12 23:28:32 -0500

    stuff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 var PORT = process.env.PORT || 3000
-var pullData = require('./app.js')
+//var pullData = require('./app.js')
 
 app.get('/', function (req, res){
 	res.send('Auction House ROI')
